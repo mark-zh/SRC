5f4ae4d4f7150ce46a171ba1a6d1f295197c6f98
mark.zhang <38938010+mark-zh@users.noreply.github.com>
2020-08-25 17:17:12 +0800

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -3,6 +3,4 @@ for i in $(cat baidu.txt); do
 	python3 ~/recon/tools/Sublist3r/sublist3r.py -d $i | tee -a $i.txt
 	~/recon/tools/assetfinder --subs-only $i | tee -a $i.txt
 	sort -u $i.txt -o $i.txt
-	python3 ~/recon/tools/ESD-1.0/ESD.py $i
-	cd ~/recon/tools/subDomainsBrute && python3 subDomainsBrute.py $i --full
 done
