65a2afe6cb1907448ba2dce2ad79b6fd63683f05
mark.zhang <38938010+mark-zh@users.noreply.github.com>
2020-08-25 17:21:34 +0800

    Update enum.sh

--- enum.sh
+++ enum.sh
@@ -1,6 +1,6 @@
 for i in $(cat baidu.txt); do
-	~/recon/tools/subfinder -d $i -o $i.txt
-	python3 ~/recon/tools/Sublist3r/sublist3r.py -d $i | tee -a $i.txt
-	~/recon/tools/assetfinder --subs-only $i | tee -a $i.txt
-	sort -u $i.txt -o $i.txt
+    ~/recon/tools/subfinder -d $i -o $i.txt
+    python3 ~/recon/tools/Sublist3r/sublist3r.py -d $i | tee -a $i.txt
+    ~/recon/tools/assetfinder --subs-only $i | tee -a $i.txt
+    sort -u $i.txt -o $i.txt
 done
